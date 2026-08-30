81dbac8b5569e71155b1dc54898aa12a143ea976
Gorden <gordenm82@googlemail.com>
2026-08-30 20:55:45 +0200

    Set RC4 informational version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0-rc.3")]
+[assembly: AssemblyInformationalVersion("4.0.0-rc.4")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
